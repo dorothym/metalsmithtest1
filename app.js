5543fbec164009ec946a89a68a932f753377cad2
dorothym <dorothy.v.moore@gmail.com>
2016-04-26 11:47:33 -0400

    backing up before moving from CLI to JS usage of metalsmith

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-app.js
+var express = require('express'),
+	metalsmith = require('metalsmith'),
+	// branch = require('metalsmith-branch'),
+	collections = require('metalsmith-collections'),
+	// excerpts = require('metalsmith-excerpts'),
+	markdown = require('metalsmith-markdown'),
+	permalinks = require('metalsmith-permalinks'),
+	// serve = require('metalsmith-serve'),
+	templates = require('metalsmith-templates');
+	// watch = require('metalsmith-watch'),
+	// moment = require('moment');
+
+var app = express();
+var port = 3030;
+
+app.use('/posts',express.static('build'));
+
+app.listen(port, function() {
+	console.log("Listening on port",port)
+}); 
+
+
